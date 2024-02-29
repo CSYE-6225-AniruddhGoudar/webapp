d8366724ab267384f900a344e8bcf268d04c3807
Anirudh Goudar <anirudhgoudar@Anirudhs-MacBook-Pro.local>
2024-02-28 19:23:41 -0500

    changing env deletion

--- systemD.sh
+++ systemD.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-# Install node modules
+# Install node modules for webapp application
 cd /opt/csye6225/webapp || exit
 sudo npm install
 
-# Copy systemd service file
+# Copy systemd file
 sudo cp /tmp/webapp.service /etc/systemd/system/
 
-# Final permission changes
+#  permission changes to group(csye6225) and user
 sudo chown csye6225:csye6225 /etc/systemd/system/webapp.service
 sudo chmod 750 /etc/systemd/system/webapp.service
 sudo chown -R csye6225:csye6225 /opt/csye6225/
 sudo chmod -R 750 /opt/csye6225/webapp
 
-# Reload systemd
+
 sudo systemctl daemon-reload
 
-# Enable and start the service
 sudo systemctl enable webapp
 sudo systemctl start webapp
 sudo systemctl status webapp
 
-# Install rsyslog for audit logs
+# audit logs
 sudo yum install -y rsyslog
 sudo systemctl daemon-reload
