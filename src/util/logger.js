c912c7a1c6a690869aa5132390e80a22406f25c4
Anirudh Goudar <anirudhgoudar@Anirudhs-MacBook-Pro.local>
2024-03-20 02:29:41 -0400

    adding loggers

--- src/util/logger.js
+++ src/util/logger.js
@@ -9,7 +9,7 @@ const logger = winston.createLogger({
     ),
     transports: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: process.env.WEBAPP_LOG_PATH ?? '/var/log/webapp/csye6225.log'})
+      new winston.transports.File({ filename: process.env.WEBAPP_LOG_PATH ?? '/var/log/webappApplication/webapp.log'})
     ]
 });
 
